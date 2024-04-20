3c2eccfe52da53a8c1fd5255dd403a97b51b2d99
Rey Jean Joy Lumapas <78730-22248454@SERVER2.DOMAIN>
2024-04-20 12:33:11 +0800

    activtity 5-6

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -6,24 +6,25 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAmtmcG2daTD1TKwTu62MrCuJtSx4PIbwQ",
+  authDomain: "activity5-6-96211.firebaseapp.com",
+  projectId: "activity5-6-96211",
+  storageBucket: "activity5-6-96211.appspot.com",
+  messagingSenderId: "867049629786",
+  appId: "1:867049629786:web:db9d1c5dd58e3a240e1063",
+  measurementId: "G-2429W98JN5"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
+
 export const db = getFirestore(app);
+
 
 export const storage = getStorage(app);
